d2b06b9ff46b84ae4bb4c330cf49b930c856d8bc
GilangIRSYD <58033323+GilangIRSYD@users.noreply.github.com>
2020-04-13 00:24:33 +0700

    Fixed some Bugs

--- app/src/main/java/com/rod/rodguild/Splashscreen.java
+++ app/src/main/java/com/rod/rodguild/Splashscreen.java
@@ -2,13 +2,25 @@ package com.rod.rodguild;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
+        setContentView(R.layout.spalsh_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this,Sign_In.class));
+                finish();
+            }
+        },2000);
     }
+
+
 }
