c53fd51c423c4d73cd27ed03d9bb529cd89d740d
GilangIRSYD <58033323+GilangIRSYD@users.noreply.github.com>
2020-04-12 23:04:21 +0700

    Fixed some Bugs

--- app/src/main/java/com/rod/rodguild/Splashscreen.java
+++ app/src/main/java/com/rod/rodguild/Splashscreen.java
@@ -9,6 +9,6 @@ public class Splashscreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Splashscreen);
+        setContentView(R.layout.splashscreen);
     }
 }
