1b58f06cf5b04b47fc9b9bffecaf51a5557d6ae7
tedy69 <tedyfazrin7@gmail.com>
2020-04-14 17:25:47 +0700

    Fix Bug Updated

--- app/src/main/java/com/rod/rodguild/SignUp.java
+++ app/src/main/java/com/rod/rodguild/SignUp.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,9 +18,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Pattern;
+
 public class SignUp extends AppCompatActivity {
 
-    EditText username,email,password,konfirmasiPassword,status;
+    EditText username, email, password, konfirmasiPassword, status;
     Button btnDaftar;
     DatabaseReference reference;
 
@@ -41,7 +45,46 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY,MODE_PRIVATE);
+                final String localUsername = username.getText().toString();
+                final String localEmail = email.getText().toString();
+                final String localPassword = password.getText().toString();
+                final String localConfirmPass = konfirmasiPassword.getText().toString();
+                final String localStatus = status.getText().toString();
+
+                if (TextUtils.isEmpty(localUsername)) {
+                    Toast.makeText(getApplicationContext(), "Enter Username!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(localEmail)) {
+                    Toast.makeText(getApplicationContext(), "Enter Email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(localEmail).matches()){
+                    email.setError("Enter a valid email");
+                    email.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(localPassword)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(localConfirmPass)) {
+                    Toast.makeText(getApplicationContext(), "Enter Confirm Password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                if (TextUtils.isEmpty(localStatus)) {
+                    Toast.makeText(getApplicationContext(), "Enter Status!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(username_key, username.getText().toString());
                 editor.apply();
@@ -51,19 +94,19 @@ public class SignUp extends AppCompatActivity {
                 reference.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()){
-                            Toast.makeText(getApplicationContext(),"Username sudah dipakai", Toast.LENGTH_SHORT).show();
-                        }else{
+                        if (dataSnapshot.exists()) {
+                            Toast.makeText(getApplicationContext(), "Username sudah dipakai", Toast.LENGTH_SHORT).show();
+                        } else {
                             dataSnapshot.getRef().child("username").setValue(username.getText().toString());
                             dataSnapshot.getRef().child("email").setValue(email.getText().toString());
                             dataSnapshot.getRef().child("status").setValue(status.getText().toString());
 
                             String konfirmasi = konfirmasiPassword.getText().toString();
                             String localPassword = password.getText().toString();
-                            if(localPassword.equals(konfirmasi)){
+                            if (localPassword.equals(konfirmasi)) {
                                 dataSnapshot.getRef().child("password").setValue(password.getText().toString());
-                            }else{
-                                Toast.makeText(getApplicationContext(), "Password tidak serupa",Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Password tidak serupa", Toast.LENGTH_SHORT).show();
                             }
                         }
 
--- app/src/main/java/com/rod/rodguild/Splashscreen.java
+++ app/src/main/java/com/rod/rodguild/Splashscreen.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 
 public class Splashscreen extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
